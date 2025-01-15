41e97b6cf19160f23233da8586e496eac351ec34
Vaughn Dice <vaughn.dice@fermyon.com>
2025-01-15 16:33:57 -0700

    feat(claims.rs): update audience to Vec<String> per spec
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- src/claims.rs
+++ src/claims.rs
@@ -2,7 +2,7 @@
 
 use std::collections::BTreeMap;
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
 /// Generic [JWT claims](https://tools.ietf.org/html/rfc7519#page-8) with
 /// defined fields for registered and private claims.
@@ -25,6 +25,30 @@ impl Claims {
 
 pub type SecondsSinceEpoch = u64;
 
+// From https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.3
+// "In the general case, the "aud" value is an array of case-
+// sensitive strings, each containing a StringOrURI value.  In the
+// special case when the JWT has one audience, the "aud" value MAY be a
+// single case-sensitive string containing a StringOrURI value."
+fn parse_audience<'de, D>(deserializer: D) -> Result<Option<Vec<String>>, D::Error>
+    where
+        D: Deserializer<'de>,
+{
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    enum Audience<'a> {
+        Str(&'a str),
+        Vec(Vec<String>),
+        None,
+    }
+
+    Ok(match Audience::deserialize(deserializer)? {
+        Audience::Str(v) => Some(vec![v.to_string()]),
+        Audience::Vec(v) => Some(v),
+        Audience::None => None,
+    })
+}
+
 /// Registered claims according to the
 /// [JWT specification](https://tools.ietf.org/html/rfc7519#page-9).
 #[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
@@ -35,8 +59,8 @@ pub struct RegisteredClaims {
     #[serde(rename = "sub", skip_serializing_if = "Option::is_none")]
     pub subject: Option<String>,
 
-    #[serde(rename = "aud", skip_serializing_if = "Option::is_none")]
-    pub audience: Option<String>,
+    #[serde(rename = "aud", skip_serializing_if = "Option::is_none", deserialize_with="parse_audience", default)]
+    pub audience: Option<Vec<String>>,
 
     #[serde(rename = "exp", skip_serializing_if = "Option::is_none")]
     pub expiration: Option<SecondsSinceEpoch>,
@@ -59,9 +83,9 @@ mod tests {
     use serde_json::Value;
     use std::default::Default;
 
-    // {"iss":"mikkyang.com","exp":1302319100,"custom_claim":true}
+    // {"iss":"mikkyang.com","exp":1302319100,"aud":["audience"],"custom_claim":true}
     const ENCODED_PAYLOAD: &str =
-        "eyJpc3MiOiJtaWtreWFuZy5jb20iLCJleHAiOjEzMDIzMTkxMDAsImN1c3RvbV9jbGFpbSI6dHJ1ZX0K";
+        "eyJpc3MiOiJtaWtreWFuZy5jb20iLCJleHAiOjEzMDIzMTkxMDAsImF1ZCI6WyJhdWRpZW5jZSJdLCJjdXN0b21fY2xhaW0iOnRydWV9";
 
     #[test]
     fn registered_claims() -> Result<(), Error> {
@@ -69,6 +93,21 @@ mod tests {
 
         assert_eq!(claims.registered.issuer.unwrap(), "mikkyang.com");
         assert_eq!(claims.registered.expiration.unwrap(), 1302319100);
+        assert_eq!(claims.registered.audience.unwrap(), vec!["audience"]);
+        Ok(())
+    }
+
+    #[test]
+    fn audience_special_case() -> Result<(), Error> {
+        // {"iss":"mikkyang.com","exp":1302319100,"aud":"audience","custom_claim":true}
+        let encoded_payload: &str =
+            "eyJpc3MiOiJtaWtreWFuZy5jb20iLCJleHAiOjEzMDIzMTkxMDAsImF1ZCI6ImF1ZGllbmNlIiwiY3VzdG9tX2NsYWltIjp0cnVlfQ==";
+
+        let claims = Claims::from_base64(encoded_payload)?;
+
+        assert_eq!(claims.registered.issuer.unwrap(), "mikkyang.com");
+        assert_eq!(claims.registered.expiration.unwrap(), 1302319100);
+        assert_eq!(claims.registered.audience.unwrap(), vec!["audience"]);
         Ok(())
     }
 
